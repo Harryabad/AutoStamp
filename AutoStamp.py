1d7e76cbcf03d3ecc6be145c109807497ced92ce
Harry <harryabad94@gmail.com>
2022-10-17 15:59:45 +0100

    Update AutoStamp.py

--- AutoStamp.py
+++ AutoStamp.py
@@ -3,42 +3,43 @@
 ## My work life requires me to stamp approve several PDF documents a week.
 ## My goal was to find a way to do this all in bulk.
 
-
+# Using PyPDF2 to manipulate pdf 
+# Using os to forloop a directorys
 import PyPDF2, os
 
-directory = "Desktop\\python auto stamp test\\Stamp\\Unstamped"
+# Folder for PDF files awaiting aproval
+
+directory = "Desktop\\PDF bulk stamp\\Unstamped" 
+
 for file in os.listdir(directory):
-#    print(file)
+    #
     input_file = file
 
+    # New destination for output files & name changes
+    output_file = "Desktop\\PDF bulk stamp\\Approved\\(Approved) " + file 
 
-#input_file = "Stamp\\Unstamped\\example.pdf"
-    output_file = "Desktop\\python auto stamp test\\Stamp\Approved\\(Approved) " + file
-    watermark_file = "Desktop\\python auto stamp test\\Stamp\Stamp.pdf"
+    # Stamp to be pasted over PDF files
+    watermark_file = "Desktop\\PDF bulk stamp\\Stamp.pdf"
 
-    with open((directory + "\\"+input_file), "rb") as filehandle_input:
-        # read content of the original file
-        pdf = PyPDF2.PdfFileReader(filehandle_input)
+    with open((directory + "\\" + input_file), "rb") as pdf_input:
+
+        # Read content of the original file
+        pdf = PyPDF2.PdfFileReader(pdf_input)
         
-        with open(watermark_file, "rb") as filehandle_watermark:
-            # read content of the watermark
-            watermark = PyPDF2.PdfFileReader(filehandle_watermark)
-            
-            # get first page of the original PDF
-            first_page = pdf.getPage(0)
-            
-            # get first page of the watermark PDF
-            first_page_watermark = watermark.getPage(0)
-            
-            # merge the two pages
-            first_page.mergePage(first_page_watermark)
-            
-            # create a pdf writer object for the output file
-            pdf_writer = PyPDF2.PdfFileWriter()
-            
-            # add page
-            pdf_writer.addPage(first_page)
-            
-            with open(output_file, "wb") as filehandle_output:
+        with open(watermark_file, "rb") as watermark_input:
+            # Read watermark
+            watermark = PyPDF2.PdfFileReader(watermark_input)
+
+            # Create new blank PDF
+            new_pdf = PyPDF2.PdfFileWriter()
+
+            # Add a new Page (Setting the first page to be the same as the first of our pdf)
+            new_pdf.addPage(pdf.getPage(0))
+
+            # Merge the two pdfs (Overlay the watermark ontop of the original pdfs)
+            new_pdf.getPage(0).merge_page(watermark.getPage(0))
+        
+            # Open output file
+            with open(output_file, "wb") as pdf_output:
                 # write the watermarked file to the new file
-                pdf_writer.write(filehandle_output)
+                new_pdf.write(pdf_output)
